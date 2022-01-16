55c56160aac8b99561ee2d5e9d5428c992f5fc8b
shankar Kudimi <shank8051@gmail.com>
2022-01-16 05:39:50 +0000

    first commit

---
+++ src/Hello_world/Hello_world.java
@@ -0,0 +1,11 @@
+package Hello_world;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World!!");
+
+	}
+
+}
